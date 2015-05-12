6179447e053a4ff79737d0ebb3e026b02773141e
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2015-05-12 18:17:47 +0900

    Specify device path from command line arg

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ var async = require('async');
 
 var device = new hvcp();
 
+var args = process.argv.slice(2);
+
 async.series([
-  device.connect.bind(device, '/dev/tty.usbserial-A603A1OQ'),
+  device.connect.bind(device, args[0]),
   device.getVersion.bind(device),
   device.setCameraOrientation.bind(device, 0),
   device.detect.bind(device, {"enableImageSmall": true}),
